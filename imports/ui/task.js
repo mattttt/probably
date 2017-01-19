64d7e78c7d2ed32dc2712efcf0d6435398e5ea5b
Matt Toback <matt@raintank.io>
2017-01-19 18:51:47 -0500

    First commit of existing files

---
+++ imports/ui/task.js
@@ -0,0 +1,29 @@
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.html';
+
+Template.task.events({
+  'click .toggle-checked'() {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this._id, {
+      $set: { checked: ! this.checked },
+    });
+  },
+  'click .delete'() {
+    Tasks.remove(this._id);
+  },
+  'click .unvote'() {
+    Tasks.update(this._id, {
+      $set: { checked: ! this.checked },
+      $inc: { vote: -1 },
+    });
+  },
+  'click .vote'() {
+    Tasks.update(this._id, {
+      $set: { checked: ! this.checked },
+      $inc: { vote: 1 },
+    });
+  },
+});
