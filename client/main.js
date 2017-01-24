8ffa9427493d6d2ba292f14872df3b7de8dbc132
Matt <matt@raintank.io>
2017-01-24 12:35:37 -0500

    Added routes

--- client/main.js
+++ client/main.js
@@ -3,6 +3,8 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { Meteor } from 'meteor/meteor';
 import { Mongo} from 'meteor/mongo';
 
+import '../lib/routes.js';
+
 import '../imports/startup/accounts-config.js';
 import '../imports/ui/body.js';
 import '../imports/api/tasks.js';
